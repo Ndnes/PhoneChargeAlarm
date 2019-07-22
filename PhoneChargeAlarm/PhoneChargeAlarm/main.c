e7eba8710bb15a44b6c7688e459ffb18312f2ff7
Terje Molnes <tmolnez@gmail.com>
2019-07-22 11:39:12 +0200

    Add AtmelStudio project
    Add fresh AtmelStudio project configured for ATtiny10 mcu.

---
+++ PhoneChargeAlarm/PhoneChargeAlarm/main.c
@@ -0,0 +1,18 @@
+/*
+ * PhoneChargeAlarm.c
+ *
+ * Created: 7/22/2019 11:32:47 AM
+ * Author : Frakkmann Stasjonaer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
