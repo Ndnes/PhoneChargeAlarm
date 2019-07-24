a990cf66eaf9e6e64ef0e23c47724b2ba124402c
Terje Molnes <tmolnez@gmail.com>
2019-07-24 13:50:19 +0200

    Update main.c
    Add code scaffolding

--- PhoneChargeAlarm/PhoneChargeAlarm/main.c
+++ PhoneChargeAlarm/PhoneChargeAlarm/main.c
@@ -27,25 +27,36 @@
  *	PB3 - NC
  */
 
+
+ /****************************************
+ Macros
+ ****************************************/
 #define F_CPU 8000000UL		//Clock frequency is 8MHz.
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-#define Start_ADC			ADCSRA |= (1<<ADSC)
-#define Conv_in_progress	ADCSRA & (1<<ADIF)	 // TRUE: still converting FALSE: finished converting.
 #define ADC_value			ADCL
 
 #define LED_on				TCCR0A &= ~(1<<COM0A1)	//The brightness of the LED is controlled with PWM so
-#define LED_off				TCCR0A |= (1<<COM0A1)	//the LED should be turned on and off by 
+#define LED_off				TCCR0A |= (1<<COM0A1)	//the LED should be turned on and off by
 #define LED_toggle			TCCR0A ^= (1<<COM0A1)	//connecting/disconnecting OC0A to the port.
 
+
+ /****************************************
+ Library import
+ ****************************************/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+
+
+/****************************************
+Initialization functions
+****************************************/
 void Set_pin_direction(void)
 {
-	DDRB |= (1<<DDB0)		//Set PB0 (pin 1 LED) to output.
-	PUEB |= (1<<PUEB1)		//Enable pull-up on PB1. (Pin 2 connected to IR switch)
+	DDRB |= (1<<DDB0);		//Set PB0 (pin 1 LED) to output.
+	PUEB |= (1<<PUEB1);		//Enable pull-up on PB1. (Pin 2 connected to IR switch)
 }
-
 void ADC_init(void)
 {
 	ADMUX |= (1<<MUX1);		//Sets ADC input to PB2.
@@ -56,17 +67,37 @@ void ADC_init(void)
 
 	DIDR0 |= (1<<ADC2D);				//Disable digital input buffer for ADC pin to reduce pwr consumption.
 }
-
 void Timer_init(void)
 {
 	TCCR0A |= (1<<COM0A1);			//Clear on compare match.***
 	
 	TCCR0A |= (1<<WGM00);			//-
 	TCCR0B |= (1<<WGM02);			//Sets up the timer for 8 bit fast PWM mode.
-	TCCR0B |= (1<<CS00);			//Turn on clock with no prescaling.
 
 	OCR0AL = 0xFF;					//Write 255 to the low byte of OCR0A register. (2byte register)
+
+	TIMSK0 |= (1<<TOIE0);			//Enable overflow interrupt for tcnt0.
 }
+
+
+void (*state)(); //Function pointer
+
+/****************************************
+Function prototypes
+****************************************/
+void startup();
+void carOn_phoneOff();
+void carOn_phoneOn();
+void carOff_phoneOn();
+void carOff_phoneOff();
+
+
+static volatile uint8_t LED_brightness = 0xFF; // Set duty cycle for LED light from 0 to 255.
+static volatile uint16_t cycle_timer_top = 15000;
+volatile uint16_t cycle_timer = 0;
+volatile bool adc_done = false;
+
+bool Phone_on_pad = false; //
 
 int main(void)
 {
@@ -75,11 +106,52 @@ int main(void)
 	Timer_init();
 
 	sei();
+	TCCR0B |= (1<<CS00);			//Turn on clock with no prescaling.
 
-	uint8_t LED_brightness = 0xFF; // Set duty cycle for LED light from 0 to 255.
+	state = startup;
 
-    while (1) 
+    while (1)
     {
+		if (!adc_done)
+		{
+			adc_done = true;
+			SMCR |= (1<<SM0);		//Do an ADC conversion in ADC noise canceling mode.
+		}							//CPU is halted while conversion is taking place
+		state();
     }
 }
 
+ISR(TIM0_OVF_vect)
+{
+	OCR0AL = LED_brightness;		//Write the compare value to set brightness (0-255)
+	cycle_timer ++;
+	if (cycle_timer > cycle_timer_top)
+	{
+		cycle_timer = 0;
+		adc_done = false;
+	}
+}
+
+/*****************************************
+State functions
+*****************************************/
+void startup()
+{
+	state = carOn_phoneOff;
+}
+void carOn_phoneOff()
+{
+	
+}
+void carOn_phoneOn()
+{
+	
+}
+void carOff_phoneOn()
+{
+	
+}
+void carOff_phoneOff()
+{
+
+}
